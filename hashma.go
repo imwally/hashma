09c8631b843f7c13b51a6e1a28dbf1d15cf7324a
Wally <wally@imwally.net>
2015-12-22 00:31:17 -0500

    Finally some working code.

--- hashma.go
+++ hashma.go
@@ -5,10 +5,34 @@ import (
 	"crypto/sha1"
 	"crypto/sha256"
 	"crypto/sha512"
+	"encoding/hex"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 )
+
+func findHash(hash, digest string) bool {
+	return strings.Contains(digest, hash)
+}
+
+func Hashes(file []byte) map[string]string {
+	hashes := make(map[string]string)
+
+	md5 := md5.Sum(file)
+	hashes["md5"] = hex.EncodeToString(md5[:])
+
+	sha1 := sha1.Sum(file)
+	hashes["sha1"] = hex.EncodeToString(sha1[:])
+
+	sha256 := sha256.Sum256(file)
+	hashes["sha256"] = hex.EncodeToString(sha256[:])
+
+	sha512 := sha512.Sum512(file)
+	hashes["sha512"] = hex.EncodeToString(sha512[:])
+
+	return hashes
+}
 
 func main() {
 
@@ -18,7 +42,7 @@ func main() {
 	}
 
 	file := os.Args[1]
-	hashFile := os.Args[2]
+	digest := os.Args[2]
 
 	fb, err := ioutil.ReadFile(file)
 	if err != nil {
@@ -26,21 +50,17 @@ func main() {
 		return
 	}
 
-	hb, err := ioutil.ReadFile(hashFile)
+	db, err := ioutil.ReadFile(digest)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	md5 := md5.Sum(fb)
-	sha1 := sha1.Sum(fb)
-	sha256 := sha256.Sum256(fb)
-	sha512 := sha512.Sum512(fb)
+	hashes := Hashes(fb)
 
-	fmt.Println(string(hb))
-
-	fmt.Printf("%x\n", md5)
-	fmt.Printf("%x\n", sha1)
-	fmt.Printf("%x\n", sha256)
-	fmt.Printf("%x\n", sha512)
+	for algo, hash := range hashes {
+		if findHash(hash, string(db)) {
+			fmt.Printf("Found %s: %s\n", algo, hash)
+		}
+	}
 }
